124a943df90a66ba081c6fae540f51bde198e5f3
Johnathan Mercier <jmercier76@gmail.com>
2017-10-09 10:03:55 -0400

    Add files via upload

---
+++ whaleTalkSolution.js
@@ -0,0 +1,18 @@
+//let input = 'I\'m sorry Dave, I\'m afraid I can\'t do that';
+let input = prompt('Enter the text you want translated');
+let vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < vowels.length; j++){
+    if (input[i] === vowels[j]) {
+      resultArray.push(input[i]);
+    };
+  };
+
+  if (input[i] === 'e' || input[i] === 'u') {
+    resultArray.push(input[i]);
+  };
+};
+
+console.log(resultArray.join('').toUpperCase());
